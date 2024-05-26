0f26a3893e2695d3b0cd8e0d9c23a1c8ff4c1592
Rubenicos <44579213+Rubenicos@users.noreply.github.com>
2024-05-26 13:23:44 -0600

    Fix hologram reload

--- plugin/src/main/java/me/badbones69/crazycrates/multisupport/DecentHologramsSupport.java
+++ plugin/src/main/java/me/badbones69/crazycrates/multisupport/DecentHologramsSupport.java
@@ -1,20 +1,31 @@
 package me.badbones69.crazycrates.multisupport;
 
 import eu.decentsoftware.holograms.api.DHAPI;
-import eu.decentsoftware.holograms.api.DecentHologramsAPI;
 import eu.decentsoftware.holograms.api.holograms.Hologram;
 import me.badbones69.crazycrates.api.interfaces.HologramController;
 import me.badbones69.crazycrates.api.objects.Crate;
 import me.badbones69.crazycrates.api.objects.CrateHologram;
 import org.bukkit.block.Block;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class DecentHologramsSupport implements HologramController {
+
+    private static final Set<String> USED_HOLOGRAMS = new HashSet<>();
+
     @Override
     public void createHologram(Block block, Crate crate) {
         CrateHologram crateHologram = crate.getHologram();
         if (crateHologram.isEnabled()) {
             double height = crateHologram.getHeight();
-            final Hologram hologram = DHAPI.createHologram("crazycrates:" + crate.getName(), block.getLocation().add(.5, height, .5));
+            final String name = "crazycrates-" + crate.getName();
+            final Hologram actualHologram = DHAPI.getHologram(name);
+            if (actualHologram != null) {
+                DHAPI.removeHologram(name);
+            }
+            USED_HOLOGRAMS.add(name);
+            final Hologram hologram = DHAPI.createHologram(name, block.getLocation().add(.5, height, .5));
             for (String line : crateHologram.getMessages()) {
                 DHAPI.addHologramLine(hologram, line);
             }
@@ -23,15 +34,13 @@ public class DecentHologramsSupport implements HologramController {
 
     @Override
     public void removeHologram(Block block, Crate crate) {
-        DHAPI.removeHologram("crazycrates:" + crate.getName());
+        DHAPI.removeHologram("crazycrates-" + crate.getName());
     }
 
     @Override
     public void removeAllHolograms() {
-        for (Hologram hologram : DecentHologramsAPI.get().getHologramManager().getHolograms()) {
-            if (hologram.getName().startsWith("crazycrates:")) {
-                hologram.delete();
-            }
+        for (String name : USED_HOLOGRAMS) {
+            DHAPI.removeHologram(name);
         }
     }
 }
